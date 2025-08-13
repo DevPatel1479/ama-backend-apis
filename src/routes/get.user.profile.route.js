a29dc5ada559d7d756b6a8bbbcf3e66ccaebfde8
Dev Patel <devpatel1828king@gmail.com>
2025-08-13 16:55:41 +0530

    updated code for making live

--- src/routes/get.user.profile.route.js
+++ src/routes/get.user.profile.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {getUserProfile}  = require('../controllers/get.user.profile.controller');
+const {getUserProfile, getUserCompleteInfo}  = require('../controllers/get.user.profile.controller');
 
 
 
 router.post("/get-profile", getUserProfile);
+router.post("/get-complete-info", getUserCompleteInfo);
 
 module.exports = router;
--- src/routes/user.query.routes.js
+++ src/routes/user.query.routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { submitUserQuery, getUserQuery, updateUserQueryStatus } = require("../controllers/user.query.controller");
+const { submitUserQuery, getUserQuery, updateUserQueryStatus, getAdminUserQuery, updateAdminUserQueryStatus } = require("../controllers/user.query.controller");
 
 router.post("/submit-user-query", submitUserQuery);
 router.get("/get-user-query", getUserQuery);
 router.post("/update-query-status", updateUserQueryStatus);
+router.get("/get-admin-user-query", getAdminUserQuery);
+router.post("/update-admin-query-status", updateAdminUserQueryStatus);
 
 
 module.exports = router;
