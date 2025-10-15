22e58df709f4e7362d57736842e7788e08e6c18f
Dev Patel <devpatel1828king@gmail.com>
2025-10-15 15:41:42 +0530

    fixed the issue

--- src/routes/advocate.notification.route.js
+++ src/routes/advocate.notification.route.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const sendNotificationToAllAdvocates = require("../controllers/advocate.notification.controller");
+const {
+  sendNotificationToAllAdvocates,
+} = require("../controllers/advocate.notification.controller");
 
 router.post("/send", sendNotificationToAllAdvocates);
 
