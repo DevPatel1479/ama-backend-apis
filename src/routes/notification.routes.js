a473c059cdaec1df84d68af88bb31e31aa016a99
Dev Patel <devpatel1828king@gmail.com>
2025-11-13 23:39:50 +0530

    added endpoint for notification history

--- src/routes/notification.routes.js
+++ src/routes/notification.routes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const {
   sendTopicNotification,
   getNotificationsByRole,
+  getUserNotificationHistory,
 } = require("../controllers/notification.controller");
 
 router.post("/send-topic-notification", sendTopicNotification);
 router.get("/get-notification/:role", getNotificationsByRole);
+router.get("/history/:user_id", getUserNotificationHistory);
 
 module.exports = router;
