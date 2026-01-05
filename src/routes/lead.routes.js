81694e7993737f11caf16447aae7d88ab1240112
Dev Patel <devpatel1828king@gmail.com>
2026-01-05 23:29:03 +0530

    added V2 for lead routes

--- src/routes/lead.routes.js
+++ src/routes/lead.routes.js
@@ -3,10 +3,14 @@ const router = express.Router();
 const {
   getLeadByPhone,
   updateUserData,
+  getLeadByPhoneV2,
+  updateUserDataV2,
 } = require("../controllers/lead.controller");
 
 // GET /api/leads/:phone
 router.get("/:phone", getLeadByPhone);
 router.put("/update", updateUserData);
+router.get("/:phone/v2", getLeadByPhoneV2);
+router.put("/update/v2", updateUserDataV2);
 
 module.exports = router;
