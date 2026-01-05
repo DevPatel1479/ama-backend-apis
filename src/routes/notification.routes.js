75a5fabf144a489e1eacfcbe3a5751a0884a9333
Dev Patel <devpatel1828king@gmail.com>
2026-01-05 23:59:57 +0530

    V2 for the notification routes

--- src/routes/notification.routes.js
+++ src/routes/notification.routes.js
@@ -4,10 +4,15 @@ const {
   sendTopicNotification,
   getNotificationsByRole,
   getUserNotificationHistory,
+  sendTopicNotificationV2,
+  getUserNotificationHistoryV2,
 } = require("../controllers/notification.controller");
 
 router.post("/send-topic-notification", sendTopicNotification);
 router.get("/get-notification/:role", getNotificationsByRole);
 router.get("/history/:user_id", getUserNotificationHistory);
+
+router.post("/send-topic-notification/v2", sendTopicNotificationV2);
+router.get("/history/:user_id/v2", getUserNotificationHistoryV2);
 
 module.exports = router;
