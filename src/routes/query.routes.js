5cf161071a0a07169728b9936bfb1971de40530f
Dev Patel <devpatel1828king@gmail.com>
2026-01-05 23:47:51 +0530

    fixed the routes for user raise query for v2

--- src/routes/query.routes.js
+++ src/routes/query.routes.js
@@ -2,11 +2,16 @@ const express = require("express");
 const {
   raiseQuery,
   getQueries,
+  raiseQueryV2,
+  getQueriesV2,
 } = require("../controllers/raise.user.query.controller");
 
 const router = express.Router();
 
 router.post("/raise-query", raiseQuery);
 router.get("/get-query", getQueries);
+
+router.post("/raise-query/v2", raiseQueryV2);
+router.get("/get-query/v2", getQueriesV2);
 
 module.exports = router;
